1ef908e7a35ad18ec32bb6a5cc6bd4f2703f6653
56KbModem <25609156+56KbModem@users.noreply.github.com>
2026-05-17 19:44:55 +0200

    needed to disable pseudo-tiling entry since hyprland changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ require('config') -- Load basic config in lua/config.lua
 require('plugins/telescope') -- Load telescope fuzzy finder
 require('lsp/init') -- Load all my LSP server configs
 
-vim.lsp.enable("rust_analyzer")  -- Enable the rust-analyzer LSP
+vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("pyright")
 vim.lsp.enable("yamlls")
 vim.lsp.enable("bashls")
